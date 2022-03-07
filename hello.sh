97bb83530d0855807f892fffea0f4d4d2a8cc890
cavazana <igor.cavazana@gmail.com>
2022-03-06 22:10:36 -0300

    Versao inicial v1.0

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+echo "Olá, estamos testando nosso projeto no GITHUB!"
+echo ""
+echo ""
+echo ""
+echo "Este é o servidor `uname -n` que estou utilizando."
+echo "Hoje é dia `date +%d_%m_%y`"
+echo ""
+echo "Obrigado!"
